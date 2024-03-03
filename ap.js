41c3a32f810a4f45164a1a78f0fa24ebb0fc21a7
Caetano <122699973+ProgCaetano@users.noreply.github.com>
2024-03-03 09:16:25 -0300

    If
    Adicionado verificação para campos vazios e alerta no windows

--- ap.js
+++ ap.js
@@ -6,13 +6,18 @@ function calcular(){
     var altura = document.querySelector('#ialtura').value;
     var peso = document.getElementById('ipeso').value;
 
-    //Cálculo IMC
-    var calculo = peso/(altura**2);
+    //SE CAMPOS NÃO VAZIOS
+    if((nome !== "") && (altura !== "") && (peso !== "")) {
 
-    //MOSTRAR NA TELA
-    document.getElementById('itexto').innerHTML = `Oi ${nome}! A sua altura é ${altura} e seu peso é ${peso}, logo seu IMC é de: ${calculo}`;
+        //Cálculo IMC
+        var calculo = peso/(altura**2);
+
+        //MOSTRAR NA TELA
+        document.getElementById('itexto').innerHTML = `Oi ${nome}! A sua altura é ${altura} e seu peso é ${peso}, logo seu IMC é de: ${calculo}`;
+    } else {
+        window.alert('Preencha todos os camos!');
+    }   
 
    
 }
-
 
